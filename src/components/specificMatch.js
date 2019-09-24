4ad158f4590e911b8a842cab72e5a7877df76e62
Morgan Sorbaro <msorbaro@gmail.com>
2019-09-24 02:26:19 -0400

    adding basic styling for main page

--- src/components/specificMatch.js
+++ src/components/specificMatch.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+const img = require('./heart.png');
+
 class SpecificMatch extends Component {
   constructor(props) {
     super(props);
@@ -13,14 +15,36 @@ class SpecificMatch extends Component {
 
 
   render() {
+    const randomNum = this.calculateMatchPercetage();
+
+    const numbers = [];
+
+    for (let i = 0; i < 10; i += 1) {
+      if (randomNum / 10 > i) {
+        numbers.push(i);
+      }
+    }
+
+    const hearts = numbers.map(number =>
+      <img alt="img" src={img} style={{ width: '30px', height: '30px', margin: '10px' }} />);
+
+
     return (
-      <div>
-        <p id="MatchName">
-          Matchehs Name: {this.props.name}
-        </p>
-        <p id="percentcompatible">
-          Percent Match: {this.calculateMatchPercetage()}
-        </p>
+      <div style={{
+ backgroundColor: 'pink', padding: '2.5%', margin: '2.5%', width: '100%', display: 'flex', flexDirection: 'row', alignItems: 'center',
+}}
+      >
+        <div style={{ width: '50%' }}>
+          <p id="Match Name">
+          Match Name: {this.props.name}
+          </p>
+          <p id="percentcompatible">
+          Percent Match: {randomNum}
+          </p>
+        </div>
+        <div style={{ width: '100%', alignItems: 'right' }}>
+          {hearts}
+        </div>
       </div>
     );
   }
